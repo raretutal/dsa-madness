583e03745bd7a6b8326e82c80b26f61298d10e48
erik1928 <caliskiserik@gmail.com>
2026-03-14 11:54:57 +0800

    Added Linked LIST. Also, added some twists, where it contains a spared node which will be reused. It becomes faster because it reduces memory allocation.

---
+++ tests/sllist.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "../src/sllist.h"
+
+using namespace std;
+
+int main() {
+    SLList<int> q;
+
+    // enqueue elements
+    q.add(10);
+    q.add(20);
+    q.add(30);
+    q.add(40);
+
+    cout << "Front element: " << q.peek() << endl;
+    cout << "Queue size: " << q.size() << endl;
+
+    cout << "\nDequeuing elements:\n";
+
+    while (!q.isEmpty()) {
+        cout << q.remove() << endl;
+    }
+
+    cout << "\nQueue empty? ";
+    cout << (q.isEmpty() ? "Yes" : "No") << endl;
+return 0;
+}
